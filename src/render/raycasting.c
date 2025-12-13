b18f2e5e97e823ba509413290366da0056528dc1
lumaruyam <lapislazuli3113@gmail.com>
2025-12-13 21:46:42 +0100

    updated raycasting.c

--- src/render/raycasting.c
+++ src/render/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: lulmaruy <lulmaruy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 21:11:41 by lulmaruy          #+#    #+#             */
-/*   Updated: 2025/12/09 22:15:29 by lulmaruy         ###   ########.fr       */
+/*   Updated: 2025/12/13 21:46:22 by lulmaruy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,47 @@ static void	init_raycasting(int x, t_ray *ray, t_player *player)
 {
 	init_ray(ray);//do we need while we do it at render.c
 	ray->camera_x = 2 * x / (double)WIN_WIDTH -1;
-	ray->dir_x = player->dir_x + player->plane_x * ray->camera_x;//to be fix
-	ray->dir_y = player->dir_y + player->plane_y * ray->camera_x;//to be fix
+	ray->dir_x = player->dir_x + player->plane_x * ray->camera_x;
+	ray->dir_y = player->dir_y + player->plane_y * ray->camera_x;
+	ray->map_x = (int)player->pos_x;
+	ray->map_y = (int)player->pos_y;
+	ray->deltadist_x = fabs(1 / ray->dir_x);
+	ray->deltadist_y = fabs(1 / ray->dir_y);
+}
 
+static void	set_dda(t_ray *ray, t_player *player)
+{
+	if (ray->dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->sidedist_x = (player->pos_x - ray->mapp_x) * ray->deltadist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->sidedist_x = (player->map_x + 1.0 - player->pos_x) * ray->deltadist_x;
+	}
+	if (ray->dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->sidedist_y = (player->pos_y - ray->map_y) * ray->deltadist_y;
+	}
+	else (ray->dir_y < 0)
+	{
+		ray->step_y = 1;
+		ray->sidedist_y = (player->map_y + 1.0 - player->pos_y) * ray->deltadist_y;
+	}
+}
+
+static void	peform_dda(t_data *data, t_ray *ray)
+{
+	int	hit;
+
+	hit = 0;
+	while (hit == 0)
+	{
+		if (ray->sidedist_x < ray->sidedist_y)
+	}
 }
 
 int	raycasting(t_player *player, t_data *data)
@@ -30,6 +68,8 @@ int	raycasting(t_player *player, t_data *data)
 	ray = data->ray;
 	while (x < data->win_width)
 	{
-		init_raycasting(x, &ray, player)
+		init_raycasting(x, &ray, player);
+		set_dda(&ray, player);
+		perform_dda(data, &ray);
 	}
 }
