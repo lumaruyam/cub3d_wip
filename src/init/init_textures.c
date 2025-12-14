e8f6f9d65cd0dabe297c0440af8c4d4bef6bafae
lumaruyam <lapislazuli3113@gmail.com>
2025-12-14 19:51:27 +0100

    updated init_textures.c

--- src/init/init_textures.c
+++ src/init/init_textures.c
@@ -6,7 +6,7 @@
 /*   By: lulmaruy <lulmaruy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 19:37:58 by lulmaruy          #+#    #+#             */
-/*   Updated: 2025/12/14 19:40:32 by lulmaruy         ###   ########.fr       */
+/*   Updated: 2025/12/14 19:48:49 by lulmaruy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	init_texture(t_data *data)
 {
-	data->textures = ft_calloc(5, sizeof * data->textures);
+	data->textures = ft_calloc(5, sizeof * data->textures);//check here if there is leak
 	if (!data->textures)
 		clean_exit(data, err_msg(NULL, ERR_MALLOC, 1));
 	// WIP 1214
